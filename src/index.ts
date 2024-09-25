36cfd6a13c89c088e38eebe2bb6c62c92b72bfd4
abarmes1411 <abarmes1411@iescarrillo.es>
2024-09-25 15:00:36 +0200

    25/09/2024

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
 console.log("Hola Mundo")
+
+
+/**
+ * 1. Tipos de datos
+ * 
+ * string
+ * number - entero y decimal
+ * boolean
+ * null 
+ * undefinied
+ * 
+ */
+var nombre:string = "Jesulin"; //Variable global
+let apellido:string = "Barroso"; //Variable local
+const PI:number = 3.1415; //Variable constante
+
+
+console.log("Hola"+" Mundo soy "+nombre+" "+apellido);
+console.log(nombre, apellido);
+console.log(nombre + apellido);
+console.log(`Hola  ${nombre}`);
+
+let a:string, b:string, c:number;
+a="texto";
+b="texto2";
+c=2;
+
+//Array de Datos
+
+let alumnos1A:string[] = ["Juan","Julio","Jose"];
+let alumnos2A:string[] = ["Fran"];
+let alumnos3A:string[] = ["Julio","Jose"];
+let alumnos:string[] = [...alumnos1A, ...alumnos2A, ...alumnos3A];
+
+let valores: (string|number|boolean)[] = ["Hola", 3, "ee", false];
+
